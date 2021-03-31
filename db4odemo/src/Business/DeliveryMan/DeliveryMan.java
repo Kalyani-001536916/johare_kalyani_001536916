3b58e3a4f79c11e5ac0f220a1e4e9592e6f6b2a4
Kalyani Johare <78626538+Kalyani-001536916@users.noreply.github.com>
2021-03-31 12:53:52 -0400

    DeliveryMan and DeliveryManDirectory added

--- db4odemo/src/Business/DeliveryMan/DeliveryMan.java
+++ db4odemo/src/Business/DeliveryMan/DeliveryMan.java
@@ -5,10 +5,44 @@
  */
 package Business.DeliveryMan;
 
+import Business.EcoSystem;
+import Business.Restaurant.Restaurant;
+import Business.UserAccount.User;
+
 /**
  *
  * @author harold
  */
-public class DeliveryMan {
+public class DeliveryMan extends User{
+    private Restaurant restaurant;
+    private boolean available = true;
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    public DeliveryMan(int id){//, int restaurantId){
+        super(id);
+//        this.restaurantID=restaurantId;
+    }
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+    public void setRestaurantByID(int id){
+        this.restaurant=EcoSystem.getInstance().getRestaurantDirectory().getRestaurantById(id);
+    }
+    public int getRestaurantByID(int id){
+        return EcoSystem.getInstance().getRestaurantDirectory().getRestaurantById(id).getId();
+    }
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
     
+    @Override
+    public String toString(){
+        return this.getLastName();
+    }
 }
