3d41a2173e0251e650ad0ade90f0663507f7d0e8
Kalyani Johare <78626538+Kalyani-001536916@users.noreply.github.com>
2021-03-31 12:44:10 -0400

    Customer and CustomerDirectory added

--- db4odemo/src/Business/Customer/Customer.java
+++ db4odemo/src/Business/Customer/Customer.java
@@ -5,10 +5,37 @@
  */
 package Business.Customer;
 
+import Business.UserAccount.User;
+import Business.pojo.Order;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author harold
  */
-public class Customer {
+public class Customer extends User{
     
+    private String address;
+    private List<Order> orders;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+        this.orders = new ArrayList();
+    }
+    public Customer(int id){
+        super(id);
+    }
+    
+    public void addOrder(Order o){
+        this.orders.add(o);
+    }
+    
+    public List<Order> getOrders(){
+        return this.orders;
+    }
 }
--- db4odemo/src/Business/Customer/CustomerDirectory.java
+++ db4odemo/src/Business/Customer/CustomerDirectory.java
@@ -5,10 +5,53 @@
  */
 package Business.Customer;
 
+import Business.Admin.Admin;
+import Business.DeliveryMan.DeliveryMan;
+import Business.EcoSystem;
+import Business.Manager.Manager;
+import Business.Restaurant.Restaurant;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author harold
  */
 public class CustomerDirectory {
+    private List<Customer> customerList = new ArrayList();
     
+    public List<Customer> getCustomerList(){
+        return this.customerList;
+    }
+    
+    public Customer createCustomer(int id){
+        Customer temp = new Customer(id);
+        customerList.add(temp);
+        return temp;
+    }
+    public void updateCustomer(String firstName, String lastName, int id){
+        Customer temp=new Customer(id);
+        for(Customer m:this.getCustomerList()){//EcoSystem.getInstance().getManagerDirectory().getManagerList()){//   this.getManagerList()){
+            if(m.getId()==id){
+                //for(Restaurant r:EcoSystem.getInstance().getRestaurantDirectory().getRestaurantList()){//.getRestaurantById(Integer.parseInt(jTextField4.getText()));){
+                //    if(r.getId()==restaurantID){                        
+                        //m.setRestaurantByID(restaurantID);
+                        m.setFirstName(firstName);
+                        m.setLastName(lastName);
+                        break;
+                    }
+                }                   
+            }
+    
+    public void removeCustomer(Customer u){
+        customerList.remove(u);
+    }
+    public void removeCustomerById(int id){
+        for(Customer m : this.getCustomerList()){
+            if(m.getId()==id){
+                this.getCustomerList().remove(m);
+                return;
+            }
+        }
+    }
 }
